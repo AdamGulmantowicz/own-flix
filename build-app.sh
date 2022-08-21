73f4eb1b313e6ae8935097599a8ccf906040be99
Adam Gulmantowicz <adam200502@interia.pl>
2022-08-21 14:32:47 +0200

    Fix build for development mode

--- build-app.sh
+++ build-app.sh
@@ -1,5 +1,5 @@
 echo "Starting up building the app..."
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 docker exec server mkdir -p build
-docker exec server cp -R public build/
+docker exec server cp -R src/public build/
 echo "App was built successfully!"
